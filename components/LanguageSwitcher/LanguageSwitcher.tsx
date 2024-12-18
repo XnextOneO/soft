330f3f52940c2a3d9b654501b45cb707fbf9b056
Ilya Horbach <b00046265@lotus.asb.by>
2024-12-18 11:09:04 +0300

    test switcher done

--- components/LanguageSwitcher/LanguageSwitcher.tsx
+++ components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import { UnstyledButton } from "@mantine/core";
+import { Flex, SegmentedControl, UnstyledButton } from "@mantine/core";
 
 import { getUserLocale, setUserLocale } from "@/i18n/locale-detector";
 
 import BY from "../../public/assets/BY.svg";
 import RU from "../../public/assets/RU.svg";
+
+import classes from "./LanguageSwitcher.module.scss";
 const LanguageSwitcherButton = (): JSX.Element => {
     const [locale, setLocale] = useState("ru");
+    const flagImages = {
+        BY: BY,
+        RU: RU,
+        // Добавьте другие языки и их флаги здесь
+    };
+    const items = [{ value: "ru" }, { value: "by" }];
 
     useEffect(() => {
         const fetchLocale = async (): Promise<void> => {
@@ -24,13 +32,28 @@ const LanguageSwitcherButton = (): JSX.Element => {
     };
 
     return (
-        <UnstyledButton onClick={handleLocaleChange} h={22}>
-            {locale === "ru" ? (
-                <Image src={RU} alt="Русский" width={"44"} height={22} />
-            ) : (
-                <Image src={BY} alt="Бел" width={"44"} height={22} />
-            )}
-        </UnstyledButton>
+        // <UnstyledButton onClick={handleLocaleChange} h={22}>
+        //     {locale === "ru" ? (
+        //         <Image src={RU} alt="Русский" width={"44"} height={22} />
+        //     ) : (
+        //         <Image src={BY} alt="Бел" width={"44"} height={22} />
+        //     )}
+        // </UnstyledButton>
+        <SegmentedControl
+            value={locale}
+            onChange={handleLocaleChange}
+            radius="xl"
+            size="md"
+            data={items.map((item) => ({
+                value: item.value,
+                label: (
+                    <Flex>
+                        <Image alt="" src={flagImages[item.value.toUpperCase()]} width="30" height={20} />
+                    </Flex>
+                ),
+            }))}
+            classNames={classes}
+        />
     );
 };
 export default LanguageSwitcherButton;
