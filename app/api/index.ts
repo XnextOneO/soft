1f1ba9005ee6a770afbaf47de1bd9ee614ed63ce
b00046568 <xnextone@gmail.com>
2025-02-26 19:48:24 +0300

    401 handler

--- app/api/index.ts
+++ app/api/index.ts
@@ -10,3 +10,12 @@ export const $authHost = axios.create({
   timeout: 3_600_000,
   withCredentials: true,
 });
+$authHost.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
