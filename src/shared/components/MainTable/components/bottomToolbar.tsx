8b52b31ac8223c2d5c3d80433ff7084a48e6eebf
b00046568 <xnextone@gmail.com>
2025-03-12 16:06:07 +0300

    added table

--- src/shared/components/MainTable/components/bottomToolbar.tsx
+++ src/shared/components/MainTable/components/bottomToolbar.tsx
@@ -1,70 +1,70 @@
 import { FC } from "react";
-import { useTranslations } from "next-intl";
+import { useTranslation } from "react-i18next";
 import { Flex, Group, Pagination, Select, Text } from "@mantine/core";
 
 interface BottomToolbarProperties {
-    page: number;
-    size: number;
-    totalElements: number;
-    countPages: number;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    parameters: any;
-    setPage: (page: number) => void;
-    setSize: (size: number) => void;
+  page: number;
+  size: number;
+  totalElements: number;
+  countPages: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  parameters: any;
+  setPage: (page: number) => void;
+  setSize: (size: number) => void;
 }
 
 const BottomToolbar: FC<BottomToolbarProperties> = ({
-    page,
-    size,
-    totalElements,
-    countPages,
-    parameters,
-    setPage,
-    setSize,
+  page,
+  size,
+  totalElements,
+  countPages,
+  parameters,
+  setPage,
+  setSize,
 }) => {
-    const t = useTranslations("bottom-toolbar");
+  const { t } = useTranslation(["bottom-toolbar"]);
 
-    const handleSizeChange = (value: string | null): void => {
-        const newSize = Number(value);
-        setSize(newSize);
-        const lastPage = Math.ceil(totalElements / newSize);
-        if (page > lastPage) {
-            setPage(lastPage);
-        } else {
-            const newTotalPages = Math.ceil(totalElements / newSize);
-            if (newTotalPages > countPages) {
-                setPage(newTotalPages);
-            }
-        }
-    };
+  const handleSizeChange = (value: string | null): void => {
+    const newSize = Number(value);
+    setSize(newSize);
+    const lastPage = Math.ceil(totalElements / newSize);
+    if (page > lastPage) {
+      setPage(lastPage);
+    } else {
+      const newTotalPages = Math.ceil(totalElements / newSize);
+      if (newTotalPages > countPages) {
+        setPage(newTotalPages);
+      }
+    }
+  };
 
-    return (
-        <Flex align="center" justify={"space-between"} w={"100%"}>
-            <Text>
-                {t("records")} {(page - 1) * size + 1}–{Math.min(page * size, totalElements)} {t("from")}{" "}
-                {totalElements}
-            </Text>
-            <Group>
-                <Text>{t("rows-count")}</Text>
-                <Select
-                    w={80}
-                    placeholder={size.toString()}
-                    data={["10", "20", "50", "100"]}
-                    defaultValue="20"
-                    allowDeselect={false}
-                    value={size.toString()}
-                    onChange={(event) => handleSizeChange(event)}
-                />
-                <Pagination
-                    color="#007458"
-                    total={countPages}
-                    value={parameters.page + 1}
-                    defaultValue={parameters.page}
-                    onChange={setPage}
-                />
-            </Group>
-        </Flex>
-    );
+  return (
+    <Flex align="center" justify={"space-between"} w={"100%"}>
+      <Text>
+        {t("records")} {(page - 1) * size + 1}–
+        {Math.min(page * size, totalElements)} {t("from")} {totalElements}
+      </Text>
+      <Group>
+        <Text>{t("rows-count")}</Text>
+        <Select
+          w={80}
+          placeholder={size.toString()}
+          data={["10", "20", "50", "100"]}
+          defaultValue="20"
+          allowDeselect={false}
+          value={size.toString()}
+          onChange={(event) => handleSizeChange(event)}
+        />
+        <Pagination
+          color="#007458"
+          total={countPages}
+          value={parameters.page + 1}
+          defaultValue={parameters.page}
+          onChange={setPage}
+        />
+      </Group>
+    </Flex>
+  );
 };
 
 export default BottomToolbar;
--- src/shared/components/MainTable/components/сreateRowModalContent.tsx
+++ src/shared/components/MainTable/components/сreateRowModalContent.tsx
@@ -1,75 +1,92 @@
 import { FC } from "react";
-import { useTranslations } from "next-intl";
-import { Button, Flex, Group, Popover, Stack, Textarea, Title } from "@mantine/core";
+import { useTranslation } from "react-i18next";
+import {
+  Button,
+  Flex,
+  Group,
+  Popover,
+  Stack,
+  Textarea,
+  Title,
+} from "@mantine/core";
 import { notifications } from "@mantine/notifications";
 
 interface CreateRowModalContentProperties {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    table: any;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    row: any;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    processedColumns: any[];
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    classes: any;
-    createRowModalOpened: boolean;
-    setCreateRowModalOpened: (value: boolean) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  table: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  row: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  processedColumns: any[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  classes: any;
+  createRowModalOpened: boolean;
+  setCreateRowModalOpened: (value: boolean) => void;
 }
 
 const CreateRowModalContent: FC<CreateRowModalContentProperties> = ({
-    processedColumns,
-    classes,
-    createRowModalOpened,
-    setCreateRowModalOpened,
-    table,
+  processedColumns,
+  classes,
+  createRowModalOpened,
+  setCreateRowModalOpened,
+  table,
 }) => {
-    const t = useTranslations("create-row-modal-content");
+  const { t } = useTranslation(["create-row-modal-content"]);
 
-    const handleCreate = (): void => {
-        setCreateRowModalOpened(false);
-        // eslint-disable-next-line unicorn/no-null
-        table.setCreatingRow(null);
-        notifications.show({
-            title: "Создано успешно",
-            message: "",
-            position: "bottom-right",
-        });
-    };
+  const handleCreate = (): void => {
+    setCreateRowModalOpened(false);
+    // eslint-disable-next-line unicorn/no-null
+    table.setCreatingRow(null);
+    notifications.show({
+      title: "Создано успешно",
+      message: "",
+      position: "bottom-right",
+    });
+  };
 
-    return (
-        <Stack mah={"80vh"} gap={5}>
-            <Title order={3} pos={"sticky"} top={10} className={classes.modalTitle}>
-                {t("create-new-row")}
-            </Title>
-            {processedColumns.map((column) => (
-                <Flex direction="column" key={column.accessorKey}>
-                    <Title order={5}>{column.header}</Title>
-                    <Textarea placeholder={`Введите ${column.header}`} resize={"vertical"} autosize />
-                </Flex>
-            ))}
-            <Flex justify="flex-end" gap={20} pos={"sticky"} bottom={10}>
-                <Popover
-                    position="bottom"
-                    withArrow
-                    opened={createRowModalOpened}
-                    onClose={() => setCreateRowModalOpened(false)}
-                >
-                    <Popover.Target>
-                        <Button onClick={() => setCreateRowModalOpened(true)}>Создать</Button>
-                    </Popover.Target>
-                    <Popover.Dropdown>
-                        <span>{t("sure-to-create")}</span>
-                        <Group mt="lg">
-                            <Button variant="outline" onClick={() => setCreateRowModalOpened(false)}>
-                                {t("cancel")}
-                            </Button>
-                            <Button onClick={handleCreate}>{t("create")}</Button>
-                        </Group>
-                    </Popover.Dropdown>
-                </Popover>
-            </Flex>
-        </Stack>
-    );
+  return (
+    <Stack mah={"80vh"} gap={5}>
+      <Title order={3} pos={"sticky"} top={10} className={classes.modalTitle}>
+        {t("create-new-row")}
+      </Title>
+      {processedColumns.map((column) => (
+        <Flex direction="column" key={column.accessorKey}>
+          <Title order={5}>{column.header}</Title>
+          <Textarea
+            placeholder={`Введите ${column.header}`}
+            resize={"vertical"}
+            autosize
+          />
+        </Flex>
+      ))}
+      <Flex justify="flex-end" gap={20} pos={"sticky"} bottom={10}>
+        <Popover
+          position="bottom"
+          withArrow
+          opened={createRowModalOpened}
+          onClose={() => setCreateRowModalOpened(false)}
+        >
+          <Popover.Target>
+            <Button onClick={() => setCreateRowModalOpened(true)}>
+              Создать
+            </Button>
+          </Popover.Target>
+          <Popover.Dropdown>
+            <span>{t("sure-to-create")}</span>
+            <Group mt="lg">
+              <Button
+                variant="outline"
+                onClick={() => setCreateRowModalOpened(false)}
+              >
+                {t("cancel")}
+              </Button>
+              <Button onClick={handleCreate}>{t("create")}</Button>
+            </Group>
+          </Popover.Dropdown>
+        </Popover>
+      </Flex>
+    </Stack>
+  );
 };
 
 export default CreateRowModalContent;
