5ed5da1b18c610a1882618b8c6e3b4397c459284
b00046568 <xnextone@gmail.com>
2025-05-27 10:08:25 +0300

    small fix login card

--- src/pages/login/ui/index.tsx
+++ src/pages/login/ui/index.tsx
@@ -104,7 +104,20 @@ export const LoginPage: FC = () => {
     <div className={backgroundState}>
       <Stack gap={0}>
         <Header isBurger={false} isProfile={false} link={false} />
-        <Card w="25%" py={70} px={80} miw={510} shadow="xl" withBorder>
+        <Card
+          w=""
+          py={70}
+          px={80}
+          miw={510}
+          shadow="xl"
+          withBorder
+          radius={0}
+          style={{
+            borderTop: "0",
+            borderBottomRightRadius: "2px",
+            borderBottomLeftRadius: "2px",
+          }}
+        >
           <Group
             gap="0"
             w="100%"
