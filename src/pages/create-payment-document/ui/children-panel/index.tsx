ddfacf02290a788e5c36e6ef0fc80ee7d57f5809
b00046568 <xnextone@gmail.com>
2025-09-24 14:47:09 +0300

    added content for rf kz india

--- src/pages/create-payment-document/ui/children-panel/index.tsx
+++ src/pages/create-payment-document/ui/children-panel/index.tsx
@@ -5,12 +5,19 @@ import styles from "./index.module.scss";
 interface IChildrenPanel {
   children: React.ReactElement;
   title: string;
+  customWidth?: number;
 }
 
-export const ChildrenPanel: FC<IChildrenPanel> = ({ children, title }) => {
+export const ChildrenPanel: FC<IChildrenPanel> = ({
+  children,
+  title,
+  customWidth,
+}) => {
   return (
     <div className={styles.wrapper}>
-      <span className={styles.blockHeader}>{title}</span>
+      <span className={styles.blockHeader} style={{ width: customWidth }}>
+        {title}
+      </span>
       {children}
     </div>
   );
--- src/pages/create-payment-document/ui/index.tsx
+++ src/pages/create-payment-document/ui/index.tsx
@@ -635,7 +635,201 @@ export const CreatePaymentDocument: FC = () => {
             </ChildrenPanel>
           </div>
         </Tabs.Panel>
-        <Tabs.Panel value="rf-kz-india">Settings tab content</Tabs.Panel>
+        <Tabs.Panel value="rf-kz-india" pl={16}>
+          <div className={styles.content}>
+            <ChildrenPanel title={"Платежи в бюджет РФ"}>
+              <>
+                <Group>
+                  <PaymentDocumentInput
+                    type={"copy"}
+                    width={149}
+                    title={<span>Код операции</span>}
+                    icon={true}
+                  />
+                  <PaymentDocumentInput
+                    type={"copy"}
+                    width={314}
+                    title={<span>Код основного налогового плательщика</span>}
+                    icon={true}
+                  />
+                </Group>
+                <Group>
+                  <PaymentDocumentInput
+                    type={"copy"}
+                    width={149}
+                    title={<span>Код выплат</span>}
+                    icon={true}
+                  />
+                  <PaymentDocumentInput
+                    type={"copy"}
+                    width={149}
+                    title={<span>КПП получателя</span>}
+                    icon={false}
+                  />
+                  <PaymentDocumentInput
+                    type={"copy"}
+                    width={314}
+                    title={<span>Уникальный идентификатор платежа</span>}
+                    icon={false}
+                  />
+                </Group>
+                <Group>
+                  <PaymentDocumentInput
+                    type={"copy"}
+                    width={314}
+                    title={<span>Код бюджетной классификации</span>}
+                    icon={false}
+                  />
+                  <PaymentDocumentInput
+                    type={"copy"}
+                    width={149}
+                    title={<span>Код ОКТМО</span>}
+                    icon={false}
+                  />
+                </Group>
+                <Group>
+                  <PaymentDocumentInput
+                    title={
+                      <span>
+                        Показ налогового периода/код таможенной <br />{" "}
+                        организации
+                      </span>
+                    }
+                    icon={false}
+                    type={"copy"}
+                    width={314}
+                  />
+                  <PaymentDocumentInput
+                    title={
+                      <span>
+                        Дата налогового <br /> документа
+                      </span>
+                    }
+                    icon={true}
+                    type={"date"}
+                    width={149}
+                  />
+                  <PaymentDocumentInput
+                    title={
+                      <span>
+                        № налогового <br /> документа
+                      </span>
+                    }
+                    icon={false}
+                    type={"copy"}
+                    width={149}
+                  />
+                </Group>
+              </>
+            </ChildrenPanel>
+            <ChildrenPanel title={"Выплаты физическим лицам на территории РФ"}>
+              <PaymentDocumentInput
+                title={<span>Код вида дохода</span>}
+                icon={false}
+                type={"copy"}
+                width={149}
+              />
+            </ChildrenPanel>
+            <ChildrenPanel title={"Платежи в Казахстан"}>
+              <>
+                <Group align={"end"}>
+                  <PaymentDocumentInput
+                    title={
+                      <span>
+                        Код назначения
+                        <br /> платежа
+                      </span>
+                    }
+                    icon={false}
+                    type={"copy"}
+                    width={149}
+                  />
+                  <PaymentDocumentInput
+                    title={
+                      <span>
+                        Код отправленных <br /> средств
+                      </span>
+                    }
+                    icon={true}
+                    type={"copy"}
+                    width={149}
+                  />
+                  <PaymentDocumentInput
+                    title={<span>Код бенефициара</span>}
+                    icon={true}
+                    type={"copy"}
+                    width={149}
+                  />
+                  <PaymentDocumentInput
+                    title={
+                      <span>
+                        Код бюджетной <br />
+                        классификации
+                      </span>
+                    }
+                    icon={false}
+                    type={"copy"}
+                    width={149}
+                  />
+                </Group>
+                <Group>
+                  <PaymentDocumentInput
+                    title={<span>Тип идентификационного номера</span>}
+                    icon={true}
+                    type={"copy"}
+                    width={314}
+                  />
+                  <PaymentDocumentInput
+                    title={<span>Идентификационный номер</span>}
+                    icon={false}
+                    type={"copy"}
+                    width={314}
+                  />
+                </Group>
+              </>
+            </ChildrenPanel>
+            <Group>
+              <ChildrenPanel
+                title={"Идентификация организации: Плательщик"}
+                customWidth={314}
+              >
+                <Group>
+                  <PaymentDocumentInput
+                    width={149}
+                    title={<span>LEI</span>}
+                    type={"copy"}
+                    icon={false}
+                  />
+                  <PaymentDocumentInput
+                    width={149}
+                    title={<span>БИК</span>}
+                    type={"copy"}
+                    icon={false}
+                  />
+                </Group>
+              </ChildrenPanel>
+              <ChildrenPanel
+                title={"Идентификация организации: Бенефициар"}
+                customWidth={314}
+              >
+                <Group>
+                  <PaymentDocumentInput
+                    width={149}
+                    title={<span>LEI</span>}
+                    type={"copy"}
+                    icon={false}
+                  />
+                  <PaymentDocumentInput
+                    width={149}
+                    title={<span>БИК</span>}
+                    type={"copy"}
+                    icon={false}
+                  />
+                </Group>
+              </ChildrenPanel>
+            </Group>
+          </div>
+        </Tabs.Panel>
         <Tabs.Panel value="automatic-details">Settings tab content</Tabs.Panel>
         <Tabs.Panel value="administrative-data">
           Settings tab content
